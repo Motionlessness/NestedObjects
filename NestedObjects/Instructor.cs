1b8697047e98c76b4d183d6477f14c5c3d7cc682
Wil Crouch <william.crouch@hotmail.com>
2020-07-13 09:14:23 -0700

    Added instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{  
+    /// <summary>
+    /// Represents an individual Instructor
+    /// </summary>
+    class Instructor
+    {
+        /// <summary>
+        /// The legal full name
+        /// Example: Jake Lohng
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// The company email address
+        /// Example: JaneDoe@cptc.edu
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
